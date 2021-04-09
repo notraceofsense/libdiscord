26260bf52b63d52649ee4e9e7ff130e15ceb6880
notraceofsense <peridotrocketscientist@gmail.com>
2021-04-09 15:24:45 -0400

    i needed an etf thing so I started a thing

---
+++ src/etf/etf.h
@@ -0,0 +1,73 @@
+// hehe pragma once go brrrrrr
+#pragma once
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <limits.h>
+#include <string.h>
+
+#include "const.h"
+
+typedef struct etf_buf {
+    char * buf;
+    size_t len;
+    size_t alloc_sz
+} etf_buf;
+
+int etf_buf_write(etf_buf * des, const char * src, size_t w_length) {
+    char* buf = des->buf;
+    size_t alloc_sz = des->alloc_sz;
+    size_t length = des->len;
+
+    if(w_length + length > alloc_sz) {
+        alloc_sz = (w_length + length) * 2;
+        buf = (char *)realloc(buf, alloc_sz);
+        if(!buf) {
+            return -1;
+        }
+        des->alloc_sz = alloc_sz;
+    }
+
+    memcpy(buf + length, src, w_length);
+    length += w_length;
+
+    des->buf = buf;
+    des->length = length;
+    return 0;
+}
+
+#define ETF_APPEND(des, src, length) \
+    return etf_buf_write(des, (const char *)src, length)
+
+int etf_append_version(etf_buf * des) {
+    static unsigned char src[1] = {FORMAT_VERSION};
+    ETF_APPEND(des, src, 1);
+}
+
+int etf_append_nil(etf_buf * des) {
+    static unsigned char src[5] = {SMALL_ATOM_EXT, 3, 'n', 'i', 'l'};
+    ETF_APPEND(des, src, 5);
+}
+
+int etf_append_false(etf_buf * des) {
+    static unsigned char src[7] = {SMALL_ATOM_EXT, 5, 'f', 'a', 'l', 's', 'e'};
+    ETF_APPEND(des, src, 7);
+}
+
+int etf_append_true(etf_buf * des) {
+    static unsigned char src[6] = {SMALL_ATOM_EXT, 4, 't', 'r', 'u', 'e'};
+    ETF_APPEND(des, src, 6);
+}
+
+int etf_append_small_int(etf_buf * des, unsigned char data) {
+    unsigned char src[2] = {SMALL_INTEGER_EXT, data};
+    ETF_APPEND(des, src, 2);
+}
+
+int etf_append_int(etf_buf * des, int32_t data) {
+    unsigned char src[5];
+    src[0] = INTEGER_EXT;
+    _ETF_STORE_32(src+1, data);
+    ETF_APPEND(des, src, 5);
+}
+
